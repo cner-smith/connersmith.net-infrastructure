acd4c2e32b4770a3022b5c3b2bed0973639e40c4
conner <connersmith+cloudresume@gmail.com>
2022-10-29 22:27:03 -0500

    for the money

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
+
 
   backend "s3" {
     bucket = "connersmith-terraform"
