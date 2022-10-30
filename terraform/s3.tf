e60e9398a87712498748c487f7e4e3977f96fe86
conner <connersmith+cloudresume@gmail.com>
2022-10-29 22:19:24 -0500

    for the money

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,16 +1,72 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-
-  tags = var.common_tags
 }
+
+#Resource to attach a bucket policy to a bucket 
+resource "aws_s3_bucket_policy" "www-s3-policy" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::760268051681:root"
+            },
+            "Action": "s3:ListBucket",
+            "Resource": "arn:aws:s3:::www.connersmith.net"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::760268051681:root"
+            },
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::www.connersmith.net/*"
+        }
+    ]
+}
+EOF
 
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-
-  tags = var.common_tags
 }
+
+#Resource to attach a bucket policy to a bucket 
+resource "aws_s3_bucket_policy" "s3-policy" {
+  bucket = aws_s3_bucket.root_bucket.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::760268051681:root"
+            },
+            "Action": "s3:ListBucket",
+            "Resource": "arn:aws:s3:::connersmith.net"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::760268051681:root"
+            },
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::connersmith.net/*"
+        }
+    ]
+}
+EOF
 
 resource "aws_s3_bucket_website_configuration" "www_bucket_config" {
   bucket = "www.${var.bucket_name}"
