aa64664ba8488c3d0e8e0e3c779e7bf6741ffe15
conner <connersmith+cloudresume@gmail.com>
2022-10-29 22:39:16 -0500

    for the monevvghvghgvhy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,18 +8,20 @@ terraform {
 
   required_version = ">=0.14.9" 
 
-  backend "s3" {
-    bucket = "connersmith-terraform"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+    backend "s3" {
+      bucket = "connersmith-terraform"
+      key    = "dev/terraform.tfstate"
+      region = "us-east-1"
+    }
 }
 
 provider "aws" {
+  version = "~>3.0"
   region = "us-east-1"
 }
 
 provider "aws" {
   alias  = "acm_provider"
+  version = "~>3.0"
   region = "us-east-1"
 }
