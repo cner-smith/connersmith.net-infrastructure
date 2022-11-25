6d3bedf70f76a173c4e7792a78109d77a97f976a
conner <connersmith+cloudresume@gmail.com>
2022-11-25 13:10:35 -0600

    i fixed it fingies crossed

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,5 +1,5 @@
-resource "aws_route53_zone" "main" {
-   for_each = {
+resource "aws_route53_record" "main" {
+    for_each = {
         for dvo in aws_acm_certificate.connersmith_acm.domain_validation_options : dvo.domain_name => {
             name = dvo.resource_record_name
             record = dvo.resource_record_value
@@ -12,28 +12,4 @@ resource "aws_route53_zone" "main" {
     ttl = 60
     type = each.value.type
     zone_id = aws_route53_zone.main.zone_id
-}
-
-resource "aws_route53_record" "root-a" {
-  zone_id = aws_route53_zone.main.zone_id
-  name = var.domain_name
-  type = "A"
-
-  alias {
-    name = aws_cloudfront_distribution.root_s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "www-a" {
-  zone_id = aws_route53_zone.main.zone_id
-  name = "www.${var.domain_name}"
-  type = "A"
-
-  alias {
-    name = aws_cloudfront_distribution.www_s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
 }
