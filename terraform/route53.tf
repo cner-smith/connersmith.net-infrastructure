0ada66f5900ab3618a5ecaa591604d6004e5e6ee
conner <connersmith+cloudresume@gmail.com>
2022-11-25 13:00:36 -0600

    i fixed it fingies crossed

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,6 +1,17 @@
 resource "aws_route53_zone" "main" {
-  name = var.domain_name
-  tags = var.common_tags
+   for_each = {
+        for dvo in aws_acm_certificate.connersmith_acm.domain_validation_options : dvo.domain_name => {
+            name = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type = dvo.resource_record_type
+        }
+    }
+    allow_overwrite = true
+    name = each.value.name
+    records = [each.value.record]
+    ttl = 60
+    type = each.value.type
+    zone_id = aws_route53_zone.main.zone_id
 }
 
 resource "aws_route53_record" "root-a" {
