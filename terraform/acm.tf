597cdde80aa3a9c86564632d194c9050e3e74280
conner <connersmith+cloudresume@gmail.com>
2022-12-06 11:26:11 -0600

    post ai

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -3,6 +3,7 @@ resource "aws_acm_certificate" "connersmith_acm" {
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
+  depends_on = [provider.aws.acm_provider]
 
   tags = {
     Environment = "dev"
@@ -20,4 +21,6 @@ resource "aws_acm_certificate_validation" "validation" {
   provider                = aws.acm_provider
   certificate_arn         = aws_acm_certificate.connersmith_acm.arn
   validation_record_fqdns = [for record in aws_route53_record.main : record.fqdn]
+  depends_on = [provider.aws.acm_provider]
+  depends_on = [aws_acm_certificate.connersmith_acm]
 }
