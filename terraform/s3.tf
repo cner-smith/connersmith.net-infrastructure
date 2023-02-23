f4095f5634b4cfb5b30bae2ea717394ca15389d8
conner <connersmith+cloudresume@gmail.com>
2023-02-22 21:20:58 -0600

    oh god here we go

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -116,9 +116,9 @@ resource "aws_cloudfront_distribution" "website" {
 }
 
 # Create a Route53 hosted zone for the domain.
-#resource "aws_route53_zone" "primary" {
-#  name = var.domain_name
-#}
+resource "aws_route53_zone" "primary" {
+  name = var.domain_name
+}
 
 # Create an ACM certificate for the domain.
 resource "aws_acm_certificate" "primary" {
