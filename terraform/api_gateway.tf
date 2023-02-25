ff6224c800bddd0164a239ae52aa54f1ebd67f11
conner <connersmith+cloudresume@gmail.com>
2023-02-24 18:41:59 -0600

    fixes

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_method" "visitor_count_get" {
 
 resource "aws_api_gateway_integration" "visitor_count_integration" {
   rest_api_id             = aws_api_gateway_rest_api.visitor_count_api.id
-  resource_id             = aws_api_gateway_resource.visitor_count_resource.resource_id
+  resource_id             = aws_api_gateway_resource.visitor_count_resource.id
   http_method             = aws_api_gateway_method.visitor_count_get.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -33,7 +33,7 @@ resource "aws_api_gateway_method" "proxy_root" {
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
+  rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
   resource_id = aws_api_gateway_method.proxy_root.resource_id
   http_method = aws_api_gateway_method.proxy_root.http_method
 
