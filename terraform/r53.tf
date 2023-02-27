3fd86f8e66a79c22be74483fc04a84f588b86857
conner <connersmith+cloudresume@gmail.com>
2023-02-26 18:27:19 -0600

    boss hogg

--- terraform/r53.tf
+++ terraform/r53.tf
@@ -13,11 +13,11 @@ resource "aws_route53_record" "website" {
 
 resource "aws_route53_record" "api_record" {
   zone_id = var.aws_route53_zone_id
-  name    = aws_api_gateway_domain_name.api.domain_name
+  name    = aws_api_gateway_domain_name.api.cloudfront_domain_name
   type    = "A"
 
   alias {
-    name                   = "api.${var.domain_name}"
+    name                   = aws_api_gateway_domain_name.api.domain_name
     zone_id                = var.aws_route53_zone_id
     evaluate_target_health = false
   }
