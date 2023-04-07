4af01465e0f15d4246c6f4c02f8d845ca19c0183
conner <connersmith+cloudresume@gmail.com>
2023-04-07 18:08:52 -0500

    javascript april 7

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,7 @@ async function get_visitors() {
     // call post api request function
     //await post_visitor();
     try {
-        let response = await fetch('https://api.connersmith.net/Prod', {
+        let response = await fetch('https://def7wo8du5.execute-api.us-east-1.amazonaws.com/Dev/', {
             method: 'GET'
         });
         let data = await response.json()
