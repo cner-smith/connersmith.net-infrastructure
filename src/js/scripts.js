58d0607b1fd79b99ab0a4cba8ae48edcae457929
conner <connersmith+cloudresume@gmail.com>
2023-04-07 18:44:45 -0500

    cors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,20 @@
 // GET API REQUEST
 async function get_visitors() {
-    // call post api request function
-    //await post_visitor();
     try {
         let response = await fetch('https://def7wo8du5.execute-api.us-east-1.amazonaws.com/Dev/', {
             method: 'GET'
         });
-        let data = await response.json()
-        document.getElementById("visitors").innerHTML = data['hits'] + " visits.";
+        let data = await response.json();
+        if (data.hasOwnProperty('hits')) {
+            document.getElementById("visitors").innerHTML = data['connersmith.net'] + " visits.";
+        } else {
+            console.error('Response from API is missing "hits" attribute.');
+        }
         console.log(data);
         return data;
     } catch (err) {
         console.error(err);
     }
 }
-
 
 get_visitors();
