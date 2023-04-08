ec9440e8894428f687b37a6e3409d14270018ac3
conner <connersmith+cloudresume@gmail.com>
2023-04-08 18:57:53 -0500

    spoopy

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -22,9 +22,9 @@ resource "aws_api_gateway_method_response" "cors_method_response_200" {
   http_method = aws_api_gateway_method.visitor_count_get.http_method
   status_code = "200"
    response_parameters = {
-      "method.response.header.Access-Control-Allow-Origin" = "'https://${var.domain_name}'",
-      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-      "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+      "method.response.header.Access-Control-Allow-Origin" = true,
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true
     }
   response_models = {
     "application/json" = aws_api_gateway_model.visitor_count_model.name
@@ -105,6 +105,11 @@ resource "aws_api_gateway_integration_response" "visitor_count_integration_respo
   resource_id = aws_api_gateway_resource.visitor_count_resource.id
   http_method = aws_api_gateway_method.visitor_count_get.http_method
   status_code = "200"
+  response_parameters = {
+      "method.response.header.Access-Control-Allow-Origin" = "'https://${var.domain_name}'",
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+    }
   response_templates = {
     "application/json" = jsonencode({ hits = "$context.authorizer.claims.hits" })
   }
