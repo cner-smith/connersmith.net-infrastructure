d31336c5dcf6d2bf1a589ae86084a7a7c36029f6
conner <connersmith+cloudresume@gmail.com>
2023-04-08 09:27:47 -0500

    saturday

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,13 @@
 # Configure the AWS provider.
 provider "aws" {
   region = var.aws_region
+
+  assume_role {
+    # The role ARN within Account B to AssumeRole into. Created in step 1.
+    role_arn    = "arn:aws:iam::760268051681:role/github-actions-role"
+    # (Optional) The external ID created in step 1c.
+    external_id = "githubactions"
+  }
 }
 
 provider "aws" {
