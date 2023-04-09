0036481f2dd45b3f13dd96bfe47389ad20a47fe4
conner <connersmith+cloudresume@gmail.com>
2023-04-09 02:30:38 -0500

    what you want girl

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -67,6 +67,8 @@ resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
   resource_id = aws_api_gateway_method.proxy_root.resource_id
   http_method = aws_api_gateway_method.proxy_root.http_method
+  
+  credentials             = aws_iam_role.api_gateway_execution_role.arn
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
