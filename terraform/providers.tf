1e64ede1e90a42d145566ec1cf498b5a94d9c226
Conner Smith <cner.smith@gmail.com>
2026-03-20 01:48:53 -0500

    fixing zipping automation

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,9 @@
-# Configure the AWS provider.
 terraform {
   required_version = ">= 1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.56"
     }
   }
 }
